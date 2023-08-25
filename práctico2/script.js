a44fb2cbeeda175a60c1e789afdb5e9a921bb280
MartinL9 <leonardomartin9@hotmail.com>
2023-08-24 22:20:05 -0300

    Agregue funciones a botones e historial

--- práctico2/script.js
+++ práctico2/script.js
@@ -2,18 +2,26 @@ const pInput = document.getElementById('pInput');
 const iconInput = document.getElementById('iconInput');
 const textInput = document.getElementById('textInput');
 const submitBtn = document.getElementById('submitBtn');
+const restartBtn = document.getElementById('restartBtn');
 const outputDiv = document.getElementById('output');
 const btnPiedra = document.getElementById('piedra');
 const btnPapel = document.getElementById('papel');
 const btnTijera = document.getElementById('tijera');
 const scoreSp = document.getElementById('score');
 const resultP = document.getElementById('result');
+const spanWin = document.getElementById('spanWin');
+const spanLose = document.getElementById('spanLose');
+const spanTotal = document.getElementById('spanTotal');
+const resetAllInfo = document.getElementById('resetAll');
 const bannedWords = ['asesinato', 'masacre', 'suicido', 'canibal', 'decapitar', 'matar', 'cadaver', 'matanza', 'crucificado','fascista', 'nazi', 'esclavo','hitler']
 
+let userName = '';
 let userScore = 0;
 let comScore = 0;
-let userName = '';
 let totalRounds = 0;
+let userWins = 0;
+let userLosses = 0;
+let totalGames = 0;
 
 disableButtonsChoice();
 
@@ -27,6 +35,44 @@ function enableButtonsChoice() {
     btnPapel.removeAttribute('disabled');
     btnPiedra.removeAttribute('disabled');
     btnTijera.removeAttribute('disabled');
+}
+
+function showResetBtn() {
+    restartBtn.style.display = 'block';
+}
+
+function clearResult() {
+    while(resultP.firstChild) {
+        resultP.removeChild(resultP.firstChild);
+    }
+}
+
+function resetGame() {
+    userScore = 0;
+    comScore = 0;
+    totalRounds = 0;
+    scoreSp.textContent = '0 - 0';
+    resultP.textContent = '';
+    restartBtn.style.display = 'none';
+    enableButtonsChoice();
+}
+
+function resetAll() {
+    resetGame();
+    userName = '';
+    userWins = 0;
+    userLosses = 0;
+    totalGames = 0;
+    spanWin.innerHTML = '<i class="fa-solid fa-crown crown"></i>';
+    spanLose.innerHTML = '<i class="fa-solid fa-x x"></i>';
+    spanTotal.innerHTML = '';
+    outputDiv.textContent = 'TU';
+    pInput.style.display = 'inline';
+    iconInput.style.verticalAlign = 'middle';
+    iconInput.style.marginTop = '27px';
+    iconInput.style.display = 'inline-block';
+    textInput.style.display = 'flex';
+    submitBtn.style.display = 'flex';
 }
 
 function userSubmission() {
@@ -88,31 +134,57 @@ function detWinner(resultPlayer, resultComp) {
     }
 }
 
+function detResultWin(playerResult, compResult) {
+    if(playerResult > compResult) {
+        return userName;
+    } else {
+        return 'Computadora'
+    }
+}
+
+let consecResults = 0;
+
 function playGame() {
-        let compPlay = comChoice();
-        let result = detWinner(compPlay, userChoice);
+    let compPlay = comChoice();
+    let result = detWinner(compPlay, userChoice);
 
-        resultP.textContent += 'El resultado fue: ' + result + '\n';
+    clearResult();
 
-        if(result === 'Ganaste') {
-            userScore++;
-        } else if(result === 'Perdiste') {
-            comScore++;
-        } else if(result === 'Empate') {
-            return;
-        }
+    if (totalRounds < 4) {
+        resultP.textContent += 'El resultado fue: ' + result;
+    }
+
+    if(result === 'Ganaste') {
+        userScore++;
+    } else if(result === 'Perdiste') {
+        comScore++;
+    } else if(result === 'Empate') {
+        return;
+    }
     
     scoreSp.textContent = `${userScore} - ${comScore}`;
     totalRounds++;
 
-    if (totalRounds === 5) {
-        resultP.textContent += `El juego ha terminado. Puntuación final para ${userName}: ${userScore} - ${comScore} Computadora.\nReinicie el juego para volver a jugar.`;
+    if(totalRounds === 5) {
+        resultP.innerHTML = `El ganador fue: ` + detResultWin(userScore, comScore) + `<br>El juego ha terminado, Reinicie el juego para volver a jugar.`;
         disableButtonsChoice();
+        showResetBtn();
+        totalGames += 1;
+        spanTotal.innerHTML = ` ${totalGames}`;
+
+        if(detResultWin(userScore, comScore) === userName) {
+            userWins++;
+            spanWin.innerHTML = `<i class="fa-solid fa-crown crown"></i> ${userWins}`;
+        } else {
+            userLosses++;
+            spanLose.innerHTML = `<i class="fa-solid fa-x x"></i> ${userLosses}`;
+        }
     }
-    
 }
 
 submitBtn.addEventListener('click', userSubmission); 
 btnPiedra.addEventListener('click', () => playerChoice('piedra'));
 btnPapel.addEventListener('click', () => playerChoice('papel'));
 btnTijera.addEventListener('click', () => playerChoice('tijera'));
+restartBtn.addEventListener('click', resetGame);
+resetAllInfo.addEventListener('click', resetAll);
